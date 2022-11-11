6f5cb13fbeb624f6c67d8a8846b81ae17a3ea0f5
aog1108 <aog1102@naver.com>
2022-11-11 18:36:35 +0900

    Refactor : Implement Builder class
    Change visit functions of BlackCalculator class to those of Builder class.
    Add definition of initializer of BlackCalculator.
    Use visitor pattern in BlackCalculator::initialize function.
    The Builder class has a member variable which is a reference of
    BlackCalculator.

--- Source/PricingEngine/BlackCalculator.cpp
+++ Source/PricingEngine/BlackCalculator.cpp
@@ -20,40 +20,41 @@ private:
 	BlackCalculator& black_;
 };
 
+BlackCalculator::BlackCalculator(const std::shared_ptr<StrikedTypePayoff>& payoff, double forward, double discount, double stddev, double variance)
+	: forward_(forward), discount_(discount), stddev_(stddev), variance_(variance) { initialize(payoff); }
+
 void BlackCalculator::initialize(const std::shared_ptr<StrikedTypePayoff>& payoff)
 {
 	d1_ = (std::log(forward_ / strike_) + 0.5 * variance_) / stddev_;
 	d2_ = (d1_ - stddev_);
 	Nd1_ = normcdf(payoff->optionType() * d1_), Nd2_ = normcdf(payoff->optionType() * d2_);
 	nd1_ = normpdf(d1_), nd2_ = normpdf(d2_);
+
+	Builder builder(*this);
+	payoff->accept(builder);
 }
 
-void BlackCalculator::visit(Payoff& payoff)
+void BlackCalculator::Builder::visit(Payoff& payoff)
 {
 	throw std::runtime_error("Unsupported payoff time.");
 }
 
-void BlackCalculator::visit(PlainVanillaPayoff& payoff)
+void BlackCalculator::Builder::visit(PlainVanillaPayoff& payoff)
 {
-	alpha_ = payoff.optionType() * Nd1_, beta_ = payoff.optionType() * Nd2_;
+	black_.alpha_ = payoff.optionType() * black_.Nd1_, black_.beta_ = payoff.optionType() * black_.Nd2_;
 }
 
-void BlackCalculator::visit(CashOrNothingPayoff& payoff)
+void BlackCalculator::Builder::visit(CashOrNothingPayoff& payoff)
 {
-	alpha_ = 0.0, beta_ = payoff.cashpayoff() * Nd2_;
+	black_.alpha_ = 0.0, black_.beta_ = payoff.cashpayoff() * black_.Nd2_;
 }
 
-void BlackCalculator::visit(AssetOrNothingPayoff& payoff)
+void BlackCalculator::Builder::visit(AssetOrNothingPayoff& payoff)
 {
-	alpha_ = payoff.optionType() * Nd1_, beta_ = 0.0;
+	black_.alpha_ = payoff.optionType() * black_.Nd1_, black_.beta_ = 0.0;
 }
 
 double BlackCalculator::value() const
 {
 	return discount_ * (forward_ * alpha_ - strike_ * beta_);
-}
-
-double BlackCalculator::delta(double S) const
-{
-
 }

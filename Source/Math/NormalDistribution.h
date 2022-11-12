17580e98aaaeca38dbbb80d0ee48ea760657d11e
aog1108 <aog1102@naver.com>
2022-11-12 19:35:56 +0900

    Fix : Do not use singleton in normal distribution.
    Singleton pattern cannot implement initialization with parameters.
    Implement new class NormalDistribution by using
    boost::math::normal_distribution<double>.

--- Source/Math/NormalDistribution.h
+++ Source/Math/NormalDistribution.h
@@ -1,14 +1,18 @@
 #pragma once
 #include <boost/math/distributions/normal.hpp>
-#include <Source/DesignPattern/Singleton.h>
 
-class NormalDistribution : private Singleton<boost::math::normal_distribution<double> > {
+class NormalDistribution {
 public:
-	static boost::math::normal_distribution<double>& get(double average, double sigma) {
-		static boost::math::normal_distribution<double> instance(average, sigma);
-		return instance;
-	}
-};
+	NormalDistribution() = default;
+	explicit NormalDistribution(double average = 0.0, double sigma = 1.0) : dist_(boost::math::normal_distribution<double>(average, sigma)) { }
+	explicit NormalDistribution(const boost::math::normal_distribution<double>& dist): dist_(dist) { }
 
-double normpdf(double x, double average = 0.0, double sigma = 1.0);
-double normcdf(double x, double average = 0.0, double sigma = 1.0);
+	double get_average() { return dist_.mean(); }
+	double get_sigma() { return dist_.standard_deviation(); }
+
+	double pdf(double x) { return boost::math::pdf(dist_, x); }
+	double cdf(double x) { return boost::math::cdf(dist_, x); }
+
+private:
+	boost::math::normal_distribution<double> dist_;
+};

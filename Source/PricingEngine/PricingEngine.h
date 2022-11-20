b1f4b33f47bba12b964c3abdf751b24674f94bf5
aog1108 <aog1102@naver.com>
2022-11-20 14:56:28 +0900

    Refactor : Change AnalyticPricingEngine architecture
    Change AnalyticPricingEngine architecture. It doesn't initialize member
    variable calculator_. Derived class initializer will initialize
    calculator_ by calling pure virtual function setupCalculator.

--- Source/PricingEngine/PricingEngine.h
+++ Source/PricingEngine/PricingEngine.h
@@ -26,16 +26,19 @@ protected:
 	Results results_;
 };
 
+//AnalyticPricingEngine은 추상클래스. 파생클래스는 Instrument와 Calculator의 템플릿을 지정해서 상속받을 것.
+//calculator_ 멤버 변수는 직접 초기화하는 변수가 아니라, 생성자에 입력되는 매개변수들을 통해 간접 초기화.
+//setupCalculator는 그를 위한 순수 가상 함수.
 template <typename T, typename AnalyticFormCalculator>
 class AnalyticPricingEngine : public PricingEngine<T> {
 public:
 	AnalyticPricingEngine() = default;
-	AnalyticPricingEngine(const T& instrument, const AnalyticFormCalculator& calculator) : PricingEngine(insturment), calculator_(calculator) { }
+	AnalyticPricingEngine(const T& instrument) : PricingEngine(instrument) { }
 
-	void calculate() = 0;
-
-	void setupCalculator(const AnalyticFormCalculator& calculator) { calculator_ = calculator; }
+	virtual void calculate() = 0;
 
 protected:
+	virtual void setupCalculator() = 0;
+
 	AnalyticFormCalculator calculator_;
 };

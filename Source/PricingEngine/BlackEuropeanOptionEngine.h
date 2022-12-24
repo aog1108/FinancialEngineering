8c3fc9ad7a4eca90f88a2834b4ae7da5a4849325
aog1108 <aog1102@naver.com>
2022-12-24 17:59:56 +0900

    Feat : Implement BlackEuropeanEngine class

--- Source/PricingEngine/BlackEuropeanOptionEngine.h
+++ Source/PricingEngine/BlackEuropeanOptionEngine.h
@@ -5,6 +5,14 @@
 
 class BlackEuropeanOptionEngine : public AnalyticPricingEngine<EuropeanOption, BlackCalculator>{
 public:
+	BlackEuropeanOptionEngine() = default;
+	BlackEuropeanOptionEngine(const std::shared_ptr<EuropeanOption>& option, double S, double r, double q, double sigma);
+	
+	void setupParameter(double S, double r, double q, double sigma);
 	void calculate();
 
+private:
+	void setupCalculator();
+
+	double S_, r_, q_, sigma_;
 };

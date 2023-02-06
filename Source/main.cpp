6f9752de56ff013a9400e94ca12f4aca0c13b9b0
aog1108 <aog1102@naver.com>
2023-02-06 18:12:35 +0900

    Temporary codes

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,12 +1,12 @@
 #include "Test.h"
 
-//FDM / MC 엔진 개발 후 PlainVanilla만 일단 가능하도록?
-//어차피 Exotic은 FDM or MC 밖에 못 쓰니까... 먼저 개발해야 될 듯?
+//FDM / MC 엔진 개발 후 PlainVanilla만 일단 가능하도록.
+//어차피 Exotic은 FDM or MC 밖에 못 쓰니까... FDM MC 먼저 개발.
 //Rate TermStructure / Vol Surface
 //FDM Implicit Explicit Crank-Nicolson Operator-Spliting
 //MC RNG Sobol sequence Brownian Bridge
 
-//Parameter 조금 변화시킨 거랑 비교해서 검증합시다.
+//Parameter 조금 변화시킨 거랑 비교해서 검증.
 
 int main() {
 	//TestDate();
@@ -17,7 +17,8 @@ int main() {
 	//TestNormalDistribution();
 	//TestBlackCalculator();
 	//TestQuantoBlackEuropeanOptionEngine();
-	TestQuantoBlackEuropeanOptionEngineGreeks();
+	//TestQuantoBlackEuropeanOptionEngineGreeks();
+	TestEstimator1D();
 
 	return 0;
 }

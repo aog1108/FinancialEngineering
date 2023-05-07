d7b74e317e73bd55d4d83bc398c0c7a521b02c48
aog1108 <aog1102@naver.com>
2023-05-07 17:18:07 +0900

    Feat : Add DateScheduleConverter class
    The DateScheduleConverter carries out converting a date schedule to
    a cashflow vector with day count basis and rate information.

--- Source/Times/Schedule/DateScheduleConverter/DateScheduleConverter.h
+++ Source/Times/Schedule/DateScheduleConverter/DateScheduleConverter.h
@@ -10,6 +10,8 @@ class DayCounter;
 
 using DateSchedule = std::map<KindsOfDate, std::vector<Date>>;
 
+//기본클래스 Cashflow로의 변환 담당.
+//Cashflow의 파생클래스가 생길 시 해당 파생클래스의 포인터로 변환하는 파생클래스를 만들어 쓸 목적으로 virtual 함수를 통해 구현.
 class DateScheduleConverter {
 public:
 	DateScheduleConverter() = default;
